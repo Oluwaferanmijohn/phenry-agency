9afccff2cf5fbcd81290403957042e286081d508
oluwatoyin oluwaferanmi john <oluwatoyinferanmijohn>
2022-06-07 22:42:57 +0100

    newly added section of the page

---
+++ app.js
@@ -0,0 +1,4 @@
+let fullname = prompt("Enter your Name");
+let age = prompt(fullname + ', how old are you ? ');
+let state = prompt(fullname + age + ' years old' + ', what is is your state of origin ');
+alert('welcome to phenry digital agency ' + fullname + ', you are ' + age + ' years old,' + ' and you are born in ' + state);
